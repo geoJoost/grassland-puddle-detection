8986eb2cc420021bbe99b4fde296908550aee3e9
ageeth1999 <135600836+ageeth1999@users.noreply.github.com>
2023-06-13 14:02:20 +0200

    Try to obtain the correct combined olygons
    Several codes tried and added, no success yet

--- scripts/01_join_anlb_brp_dataframes.py
+++ scripts/01_join_anlb_brp_dataframes.py
@@ -31,15 +31,18 @@ BRP.to_file("output/01_BRP_filtered.shp")
 # just for easy filling in the function
 filepath_ANLB = "data/Shapes/ANLB_2021.shp"
 code_list_ANLB = ['3a','3b','3c','3d']
-def filterANLB(filepath, code_list):
+Friesland = gpd.read_file("data/Shapes/Friesland_area.shp")
+
+def filterANLB(filepath, code_list, province):
     import geopandas as gpd
     ANLB = gpd.read_file(filepath)
+    ANLB = ANLB.clip(province)
     # select plasdras areas
     ANLB = ANLB[ANLB["CODE_BEHEE"].isin(code_list)]
     return ANLB
 
 # filter ANLB data and safe it as a shapefile
-ANLB=filterANLB(filepath_ANLB,code_list_ANLB)
+ANLB=filterANLB(filepath_ANLB,code_list_ANLB, Friesland)
 ANLB.to_file("output/01_ANLB_filtered.shp")
 
 BRP = gpd.read_file("output/01_BRP_filtered.shp")
@@ -47,35 +50,38 @@ ANLB = gpd.read_file("output/01_ANLB_filtered.shp")
 
 ### join BRP and ANLB data ################################################
 def joindataframes(df1, df2):
+# right gives point data, left gives all available parcels, inner gives some areas which are inundated
+# tested using       subsidised_field = joindataframes(BRP,ANLB)
     import geopandas as gpd
-    df1 = df1.geometry.apply(lambda polygon: polygon.exterior.coords)
-    df2["Centroid"] = df2.centroid
+    df2["Centroid"] = df1.centroid
     df2 = gpd.GeoDataFrame(df2, geometry= df2["Centroid"])
     df2_dropped = df2.drop(['Centroid'], axis=1)
 #     code not completely working due to some holes in the BRP data
 #     code below takes all fields within the max distance of the centroid of
 #     subsidised areas
 #     ANLB_BRP = gpd.sjoin_nearest(df1, df2_dropped,max_distance=10)
-#    ANLB_BRP = gpd.sjoin(df1, df2_dropped)
-    return df1
+    ANLB_BRP = gpd.sjoin(df1, df2_dropped, how='right')
+    return ANLB_BRP
 
-def close_holes(poly: Polygon) -> Polygon:
-        """
-        Close polygon holes by limitation to the exterior ring.
-        Args:
-            poly: Input shapely Polygon
-        Example:
-            df.geometry.apply(lambda p: close_holes(p))
-        """
-        if poly.interiors:
-            return Polygon(list(poly.exterior.coords))
-        else:
-            return poly
+### join BRP and ANLB data ################################################
+# only working when using right (left and inner does not give any visualisation results)
+# does not give the parcels, but the areas which are inundated
+# tested using       subsidised_field = joindataframes(BRP,ANLB)
+#def joindataframes(df1, df2):
+#    import geopandas as gpd
+#    df1["Centroid"] = df1.centroid
+#    df1 = gpd.GeoDataFrame(df1, geometry= df1["Centroid"])
+#    df1_dropped = df1.drop(['Centroid'], axis=1)
+#     code not completely working due to some holes in the BRP data
+#     code below takes all fields within the max distance of the centroid of
+#     subsidised areas
+#     ANLB_BRP = gpd.sjoin_nearest(df1, df2_dropped,max_distance=10)
+#    ANLB_BRP = gpd.sjoin(df1_dropped, df2, how='right')
+#    return ANLB_BRP
 
-df = df.geometry.apply(lambda p: close_holes(p))
 
 # join BRP and ANLB data and safe it as a shapefile
-subsidised_field = joindataframes(BRP,ANLB)
+subsidised_field = joindataframes(ANLB,BRP)
 subsidised_field.to_file("output/01_subsidised_field.shp")
 
 
