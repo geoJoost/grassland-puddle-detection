bf1df40ee337788e8748b123e39b4c3cf816c383
marnic23 <130738021+marnic23@users.noreply.github.com>
2023-06-28 09:54:12 +0200

    update visualisation
    dash application created
    
    Co-Authored-By: DaanLichtenberg <135601811+DaanLichtenberg@users.noreply.github.com>

--- scripts/s07_visualisation.py
+++ scripts/s07_visualisation.py
@@ -19,49 +19,18 @@ df = gpd.read_file("output/01_subsidised_field.shp")
 subsidised = df.to_crs(crs=4326)
 del df
 
-    
+
+app = Dash(__name__)
+
+app.layout = html.Div([html.H4("Puddle mapping")])
+
+
 fig = px.choropleth(subsidised, geojson= subsidised.geometry, color=subsidised["Parcel_fou"],
                     locations=subsidised.index) #hover_data=subsidised["CODE_BEHEE"])
 
 #fig.update_geos(fitbounds=subsidised.geometry, visible=False)
 #fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.show()
-
-app = Dash(__name__)
-
-app.layout = html.Div(
-    [
-        html.H4("Political candidate voting pool analysis"),
-        html.P("Select a candidate:"),
-        dcc.RadioItems(
-            id="candidate",
-            options=["Joly", "Coderre", "Bergeron"],
-            value="Coderre",
-            inline=True,
-        ),
-        dcc.Graph(id="graph"),
-    ]
-)
-
-
-@app.callback(
-    Output("graph", "figure"),
-    Input("candidate", "value"),
-)
-def display_choropleth(candidate):
-    geojson = px.data.election_geojson()
-    fig = px.choropleth(
-        df,
-        geojson=geojson,
-        color=candidate,
-        locations="district",
-        featureidkey="properties.district",
-        projection="mercator",
-        range_color=[0, 6500],
-    )
-    fig.update_geos(fitbounds="locations", visible=False)
-    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-    return fig
 
 
 if __name__ == "__main__":
