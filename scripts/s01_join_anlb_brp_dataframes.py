f10ee40cbe586b69fa9958c3dfa1ddae6b9d29c0
Petra Bardocz <petra.bardocz@gmail.com>
2023-06-29 10:58:15 +0200

    Created validation shapefile and cleaned up hard-coded parts

--- scripts/s01_join_anlb_brp_dataframes.py
+++ scripts/s01_join_anlb_brp_dataframes.py
@@ -63,20 +63,21 @@ def joindataframes(df1, df2):
 #%% Define filepaths
 
 #Input filepaths
-brp_parcels_fp = "D:/RGIC23GR10/data/Shapes/gewaspercelen_2021_S2Tiles_GWT_BF12_AHN2.shp"
-anlb_parcel_filepaths = "D:/RGIC23GR10/data/Shapes/ANLB_2021.shp"
+brp_parcels_fp = "D:/RGIC23GR10/data/Shapes/gewaspercelen_2021_S2Tiles_GWT_BF12_AHN2.shp" # BRP parcels
+anlb_parcel_filepaths = "D:/RGIC23GR10/data/Shapes/ANLB_2021.shp" # ANLB parcels
 
 #Intermediate filepaths
-filtered_anlb_fp = "D:/RGIC23GR10/data/01_ANLB_filtered.shp"
-grassland_brp_fp = "D:/RGIC23GR10/data/01_brp_grasslands.shp"
+filtered_anlb_fp = "D:/RGIC23GR10/data/01_ANLB_filtered.shp" # ANLB parcels filtered for subsidy packages 3a-b-c-d
+grassland_brp_fp = "D:/RGIC23GR10/data/01_brp_grasslands.shp" # BRP parcels filtered for grasslands
 
 #Output filepaths
-joined_parcel_fp = "D:/RGIC23GR10/output/01_subsidised_field.shp"
-brp_grass_sample_fp ='D:/RGIC23GR10/output/01_brp_dry_grass_sample.shp'
+joined_parcel_fp = "D:/RGIC23GR10/output/01_subsidised_field.shp" #not sure if used
+brp_grass_sample_fp ="D:/RGIC23GR10/output/01_brp_grassland_sample_200.shp" # 200 BRP grassland parcels which exclude the ANLB parcels
+validation_parcel_fp = "D:/RGIC23GR10/output/01_anlb_drygrass_merged.shp" # ANLB parcels merged with BRP grass only parcels for validation raster clip
 
 #%% Filter brp to graslands and write to file
 brp_parcels_gdf = gpd.read_file(brp_parcels_fp).to_crs(32631)
-grasland_brp_parcels = brp_parcels_gdf.loc[brp_parcels_gdf['cat_gewasc'] == 'Grasland']
+grasland_brp_parcels = brp_parcels_gdf[brp_parcels_gdf['cat_gewasc'] == 'Grasland']
 if not os.path.exists(grassland_brp_fp):
     grasland_brp_parcels.to_file(grassland_brp_fp)
     print(f"{grassland_brp_fp} written to file.")
@@ -105,10 +106,11 @@ else:
 #     subsidised_field.to_file(joined_parcel_fp)
 
 #%% Create dataset containing only dry grass polygons
-# Load in the ANLB-subsidy parcels and BRP parcels
+# Load in the ANLB-subsidy parcels and BRP grassland parcels
 anlb_gdf = gpd.read_file(filtered_anlb_fp).to_crs(32631)
-brp_gdf = gpd.read_file(brp_parcels_fp).to_crs(32631)
+brp_gdf = gpd.read_file(grassland_brp_fp).to_crs(32631)
 
+brp_large_parcels = brp_gdf[brp_gdf['area'] >= 30000] # Filter for parcels that are larger or equal to 30000 square meters
 
 # Clipping
 if os.path.exists(brp_grass_sample_fp):
@@ -118,7 +120,7 @@ else:
     print("BRP sampled dataset does not exist yet")
 
     # Randomly select 70,000 (about 10% of original sample, N=511,031) polygons from the BRP data
-    gdf_brp_sample = brp_gdf.sample(n=50000, random_state=1)
+    gdf_brp_sample = brp_large_parcels.sample(n=200, random_state=1)
 
     # Conduct a reverse clip to make sure both vector files do not overlap
     gdf_brp_clip = gpd.overlay(gdf_brp_sample, anlb_gdf, how='difference')
@@ -128,3 +130,15 @@ else:
     
     print("BRP sampled dataset created \n")
     
+
+# Merge ANLB and grass sample shapefiles
+# Get gdfs 
+
+dataframesList = [gdf_brp_clip, anlb_gdf]
+
+### Create validation shapefiles per date
+# Merge/Combine multiple shapefiles into one
+rdf = gpd.GeoDataFrame(pd.concat(dataframesList, ignore_index=True), crs=dataframesList[0].crs)
+ 
+#Export merged geodataframe into shapefile
+rdf.to_file(validation_parcel_fp)
